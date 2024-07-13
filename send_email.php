a309fb5206ae117a8385a1d6bfa11de2d4300d6e
danishsalmani <salmanidanish488@gmail.com>
2024-07-13 21:35:26 +0530

    updated card

--- send_email.php
+++ send_email.php
@@ -1,10 +1,15 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $to_email = "salmanidanish202@gmail.com";
-    $subject = $_POST['Subject'];
-    $message = $_POST['Message'];
-    $name = $_POST['Name'];
-    $email = $_POST['Email'];
+    $to_email = "";
+    $subject = $_POST['subject'];
+    $message = "Year: " . $_POST['year'] . "\n" .
+               "Maker: " . $_POST['maker'] . "\n" .
+               "Model: " . $_POST['model'] . "\n" .
+               "Parts: " . $_POST['parts'] . "\n" .
+               "Comments: " . $_POST['comments'] . "\n\n" .
+               $_POST['comments'];
+    $name = $_POST['first-name'] . " " . $_POST['last-name'];
+    $email = $_POST['email'];
     
     // Email headers
     $headers = "From: $name <$email>";
